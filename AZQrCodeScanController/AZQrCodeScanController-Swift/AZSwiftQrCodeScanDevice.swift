ec8effc11b9fd858a227285222a2bbee9b9bbb46
pointee <tianfengyu@foxmail.com>
2017-06-14 19:25:47 +0800

    新增：导航栏

--- AZQrCodeScanController/AZQrCodeScanController-Swift/AZSwiftQrCodeScanDevice.swift
+++ AZQrCodeScanController/AZQrCodeScanController-Swift/AZSwiftQrCodeScanDevice.swift
@@ -6,4 +6,58 @@
 //  Copyright © 2017年 Azreal. All rights reserved.
 //
 
-import Foundation
+import AVFoundation
+
+class AZSwiftQrCodeScanDevice: NSObject, AVCaptureMetadataOutputObjectsDelegate {
+    
+    private var device: AVCaptureDevice!
+    private var input: AVCaptureDeviceInput!
+    private var output: AVCaptureMetadataOutput!
+    var session: AVCaptureSession!
+    private var preview: AVCaptureVideoPreviewLayer!
+    var complete: ((String)->())?
+    
+    init(scanFrame: CGRect, layer: CALayer) {
+        super.init()
+        device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        do {
+            input = try AVCaptureDeviceInput(device: device)
+        } catch {
+            print(error)
+        }
+        output = AVCaptureMetadataOutput()
+        output.setMetadataObjectsDelegate(self, queue: .main)
+        
+        session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSessionPresetHigh
+        if session.canAddInput(input) {
+            session.addInput(input)
+        }
+        if session.canAddOutput(output) {
+            session.addOutput(output)
+        }
+        output.metadataObjectTypes = [AVMetadataObjectTypeQRCode, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code]
+        output.rectOfInterest = CGRect(x: scanFrame.origin.y/AZ_screenHeight,
+                                       y: (AZ_screenWidth-scanFrame.size.width-scanFrame.origin.x)/AZ_screenWidth,
+                                       width: scanFrame.size.height/AZ_screenHeight,
+                                       height: scanFrame.size.width/AZ_screenWidth)
+        preview = AVCaptureVideoPreviewLayer(session: session)
+        preview.videoGravity = AVLayerVideoGravityResizeAspectFill
+        preview.frame = CGRect(x: 0, y: 0, width: AZ_screenWidth, height: AZ_screenHeight)
+        layer.insertSublayer(preview, at: 0)
+        session.startRunning()
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        if metadataObjects.count > 0 {
+            session.stopRunning()
+            let metadataObject = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+            let stringValue = metadataObject.stringValue
+            guard stringValue != nil else {
+                complete?("")
+                return
+            }
+            complete?(stringValue!)
+        }
+    }
+}
--- AZQrCodeScanController/AZQrCodeScanController-Swift/AZSwiftQrCodeScanView.swift
+++ AZQrCodeScanController/AZQrCodeScanController-Swift/AZSwiftQrCodeScanView.swift
@@ -6,4 +6,105 @@
 //  Copyright © 2017年 Azreal. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AZSwiftQrCodeScanView: UIView {
+    var topCoverView = UIView()
+    var leftCoverView = UIView()
+    var rightCoverView = UIView()
+    var bottomCoverView = UIView()
+    
+    fileprivate var scanFrame: CGRect!
+    
+    var scanImageView: UIImageView! // 扫码框
+    var scanLine: UIImageView! // 扫码线
+    var introduceLabel: UILabel! // 提示文字label
+    
+    
+    /// 扫码线移动方向
+    private enum LineMoveDirect {
+        case up
+        case down
+    }
+    var timer: DispatchSourceTimer!
+    private var lineDirection = LineMoveDirect.down
+    
+    convenience init(scanFrame: CGRect) {
+        self.init(frame: CGRect(x: 0, y: 0, width: AZ_screenWidth, height: AZ_screenHeight))
+        configCoverView()
+        
+        self.scanFrame = scanFrame
+        resetCoverViewFrame()
+        configScanUI()
+        configTimer()
+    }
+    
+    /// 扫码线移动
+    private func configTimer() {
+        timer = DispatchSource.makeTimerSource(flags: .strict, queue: .main)
+        timer.scheduleRepeating(deadline: .now(), interval: .milliseconds(10))
+        weak var wkSelf = self
+        timer.setEventHandler {
+            var lineFrame = wkSelf!.scanLine.frame
+            switch wkSelf!.lineDirection {
+            case .up:
+                lineFrame.origin.y -= 1
+            case .down:
+                lineFrame.origin.y += 1
+            }
+            wkSelf?.scanLine.frame = lineFrame
+            if lineFrame.origin.y >= wkSelf!.scanFrame.origin.y + wkSelf!.scanFrame.width - lineFrame.size.height {
+                wkSelf!.lineDirection = .up
+            } else if lineFrame.origin.y <= wkSelf!.scanFrame.origin.y {
+                wkSelf!.lineDirection = .down
+            }
+        }
+        timer.resume()
+    }
+    
+    /// 扫码框/扫码线/介绍文字
+    private func configScanUI() {
+        
+        let bundlePath = Bundle(for: classForCoder).path(forResource: "AZQrCode", ofType: "bundle")!
+        let captureBundle = Bundle(path: bundlePath)!
+        
+        scanImageView = UIImageView(frame: scanFrame)
+        let bgPath = captureBundle.path(forResource: "scan_bg_pic@2x", ofType: "png")!
+        scanImageView.image = UIImage(contentsOfFile: bgPath)
+        addSubview(scanImageView)
+        
+        scanLine = UIImageView(frame: CGRect(x: scanFrame.origin.x, y: scanFrame.origin.y
+            , width: scanFrame.width, height: 2))
+        let linePath = captureBundle.path(forResource: "scan_line@2x", ofType: "png")!
+        scanLine.image = UIImage(contentsOfFile: linePath)
+        addSubview(scanLine)
+        
+        introduceLabel = UILabel(frame: CGRect(x: scanFrame.origin.x, y: scanFrame.origin.y+scanFrame.size.height+20, width: scanFrame.width, height: 40))
+        introduceLabel.numberOfLines = 0
+        introduceLabel.textAlignment = .center
+        introduceLabel.text = "将二维码/条码放入框内，即可自动扫描。"
+        introduceLabel.textColor = .white
+        introduceLabel.font = UIFont.systemFont(ofSize: 14)
+        addSubview(introduceLabel)
+    }
+    
+    /// 配置coverView
+    private func configCoverView() {
+        for item in [topCoverView, leftCoverView, bottomCoverView, rightCoverView] {
+            item.backgroundColor = UIColor(white: 0, alpha: 0.4)
+            addSubview(item)
+        }
+    }
+    
+    /// 设置遮罩层位置
+    private func resetCoverViewFrame() {
+        leftCoverView.frame = CGRect(x: 0, y: 0, width: scanFrame.origin.x, height: AZ_screenHeight)
+        topCoverView.frame = CGRect(x: scanFrame.origin.x, y: 0, width: AZ_screenWidth, height: scanFrame.origin.y)
+        rightCoverView.frame = CGRect(x: scanFrame.origin.x, y: scanFrame.height+scanFrame.origin.y, width: AZ_screenWidth, height: AZ_screenHeight)
+        bottomCoverView.frame = CGRect(x: scanFrame.width+scanFrame.origin.x, y: scanFrame.origin.y, width: AZ_screenWidth, height: scanFrame.height)
+    }
+    
+    
+    override init(frame: CGRect) { super.init(frame: frame) }
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
